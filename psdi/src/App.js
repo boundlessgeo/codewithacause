5aefe1d8d7f3fa7a7041acea39dff0bfc7169be5
Carly Langlois <cj.langlois.cl@gmail.com>
2018-02-10 14:36:20 -0600

    feature popup displays place name

--- psdi/src/App.js
+++ psdi/src/App.js
@@ -28,6 +28,45 @@ const store = createStore(combineReducers({
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 applyMiddleware(thunkMiddleware));
 
+class MarkFeaturesPopup extends SdkPopup {
+
+  constructor(props) {
+    super(props);
+    this.markFeatures = this.markFeatures.bind(this);
+  }
+
+  markFeatures(evt) {
+    const feature_ids = [];
+    const features = this.props.features;
+
+    for (let i = 0, ii = features.length; i < ii; i++) {
+      // create an array of ids to be removed from the map.
+      feature_ids.push(features[i].properties.Title);
+      // set the feature property to "marked".
+      features[i].properties.isMarked = true;
+    }
+
+    // remove the old unmarked features
+    //store.dispatch(SdkMapActions.removeFeatures('points', ['in', 'id'].concat(feature_ids)));
+    // add the new freshly marked features.
+    //store.dispatch(SdkMapActions.addFeatures('points', features));
+    // close this popup.
+    this.close(evt);
+  }
+
+  render() {
+    const feature_ids = this.props.features.map(f => f.properties.Title);
+    console.log(this.props.features)
+    return this.renderPopup((
+      <div className="sdk-popup-content">
+        <p>
+          { feature_ids.join(', ') }
+        </p>
+      </div>
+    ));
+  }
+}
+
 class App extends Component {
 
   componentDidMount() {
@@ -161,7 +200,10 @@ class App extends Component {
 
                 if (features === undefined) {
                   // no features, :( Let the user know nothing was there.
-                  map.addPopup(<SdkPopup coordinate={xy}><i>This is a popup!</i></SdkPopup>);
+                  //map.addPopup(<SdkPopup coordinate={xy}><i>This is a popup!</i></SdkPopup>);
+                } else {
+                  // Show the super advanced fun popup!
+                  map.addPopup(<MarkFeaturesPopup coordinate={xy} features={features} />);
                 }
                 });
               }}
